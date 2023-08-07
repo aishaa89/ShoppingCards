d2d6898a8b3aad2f2db75b71de7c43dfdaa12f0a
mohamed <mohamed@DESKTOP-H12ITEV>
2023-08-07 17:13:15 +0300

    Add project files.

---
+++ ShoppingCards/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCard.DataAccess.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+
+    {
+    public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> products { get; set; }
+    }
+}
